dd162f951a69fd9ceb8b7cb16ab96dc11ffe8a94
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-17 03:28:24 +0600

    input validation

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log(4);
+document.getElementById("input-container").addEventListener('keyup', function (event) {
+    if (event.target.tagName == 'INPUT') {
+        if (checkInput(event.target.value)) {
+            console.log(event.target.value);
+        }
+    }
+});
+// verify input 
+function checkInput(inputNumber) {
+    if (isNaN(inputNumber)) {   // if input is not a number  
+        console.error('NaN');
+    }
+    else if (inputNumber < 0) { // if input is negative number
+        console.error('Negative')
+    }
+    else {
+        return true;
+    }
+    return false;
+}
